0b0f916dc660238d5fab00079d2db4ea5fad11a1
Adrianne Julian Claresta <150659840+Idheid@users.noreply.github.com>
2024-05-28 22:09:02 +0700

    Update parsing_string_infix.cpp

--- parsing_string_infix.cpp
+++ parsing_string_infix.cpp
@@ -10,6 +10,12 @@ bool isOperator(char op) {
     return op == '+' || op == '-' || op == '*' || op == '/' || op == '%';
 }
 
+int Precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/' || op == '%') return 2;
+    return 0;
+}
+
 vector<string> stringToInfix(string str) {
     vector<string> infix;
     string angka;
@@ -47,6 +53,37 @@ vector<string> stringToInfix(string str) {
     return infix;
 }
 
+vector<string> infixToPostfix(const vector<string>& infix) {
+    vector<string> postfix;
+    stack<string> ops;
+
+    for (const string& token : infix) {
+        if (isdigit(token[0]) || token[0] == '.' || (token.size() > 1 && (isdigit(token[1]) || token[1] == '.'))) {
+            postfix.push_back(token);
+        } else if (token == "(") {
+            ops.push(token);
+        } else if (token == ")") {
+            while (!ops.empty() && ops.top() != "(") {
+                postfix.push_back(ops.top());
+                ops.pop();
+            }
+            if (!ops.empty()) ops.pop();
+        } else if (isOperator(token[0])) {
+            while (!ops.empty() && Precedence(ops.top()[0]) >= Precedence(token[0])) {
+                postfix.push_back(ops.top());
+                ops.pop();
+            }
+            ops.push(token);
+        }
+    }
+
+    while (!ops.empty()) {
+        postfix.push_back(ops.top());
+        ops.pop();
+    }
+
+    return postfix;
+}
 
 void printInfix(const vector<string>& infix) {
     for (size_t i = 0; i < infix.size(); ++i) {
@@ -54,6 +91,8 @@ void printInfix(const vector<string>& infix) {
     }
     cout << endl;
 }
+
+
 
 int main() {
     string str;
